a0a67d2cc20cfb03a161276450229a7f0eb3ed94
someshwarsholkar <someshwarsholkar22@gmail.com>
2024-08-27 14:29:13 +0530

    client build

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express= require("express")
 const cors= require("cors")
 const mongoose= require("mongoose")
+const path= require("path")
 const { app, httpServer } = require("./socket/Socket")
 require("dotenv").config()
 
 // const app = express()
 
 app.use(express.json())
+app.use(express.static("dist"))
 app.use(cors({origin:true,credentials:true}))
 
 app.use("/api/notes",require("./routes/todo.routes"))
 
 app.use("*",( req, res )=>{
+    // res.send(path.join(__dirname,"dist","index.html"))
     res.status(404).json({message:"Resource Not Found"})
 })
 app.use((err, req, res,next)=>{
