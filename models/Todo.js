3c744612512a1fdd13c764826b32dd2f227c7a12
someshwarsholkar <someshwarsholkar22@gmail.com>
2024-08-28 12:05:11 +0530

    Updated Todos

--- models/Todo.js
+++ models/Todo.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose")
 
 const todoSchema= new mongoose.Schema({
-    task:{type:String,required:true},
-    desc:{type:String,required:true},
-    priority:{type:String,required:true},
-    complete:{type:String,default:false},
+    task:{ type : String, required:true},
+    desc:{ type : String, required:true},
+    priority:{ type : String, required:true},
+    complete:{ type : Boolean, default:false},
 
 
 },{timestamps:true})
